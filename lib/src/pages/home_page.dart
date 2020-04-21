0fd93be209a1bf9278e8ed48c1dfa56246037ea7
josephpenafiel <josephpenafiel@gmail.com>
2020-04-21 13:57:47 -0500

    alert & avatar pages, added routing

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,7 @@
+// import 'package:components/src/pages/alert_page.dart';
+// import 'package:components/src/pages/avatar_page.dart';
 import 'package:components/src/providers/menu_provider.dart';
+import 'package:components/src/utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,20 +21,24 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (BuildContext cxt, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _itemsList(snapshot.data),
+          children: _itemsList(snapshot.data, cxt),
         );
       },
     );
   }
 
-  List<Widget> _itemsList(List<dynamic> data) {
+  List<Widget> _itemsList(List<dynamic> data, BuildContext cxt) {
     final List<Widget> opts = [];
     data.forEach((d) {
       final tmpWidget = ListTile(
         title: Text(d['texto']),
-        leading: Icon(Icons.account_circle, color: Colors.blue),
+        leading: getIcon(d['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blueAccent),
-        onTap: () {},
+        onTap: () {
+          Navigator.pushNamed(cxt, d['ruta']);
+          // final route = MaterialPageRoute(builder: (cxt) => AlertPage());
+          // Navigator.push(cxt, route);
+        },
       );
 
       opts..add(tmpWidget)..add(Divider());
